e16b110ab4a7945c89001be5792da6c02b2111b1
Corey Mcmahon <contact@coreymcmahon.com>
2017-10-28 17:51:07 +0700

    Updates

--- docker/postgres/init-db.sh
+++
@@ -1,5 +0,0 @@
-psql -U postgres -c "CREATE DATABASE foodkitdb;";
-psql -U postgres -c "CREATE ROLE foodkit WITH LOGIN PASSWORD 'secret';" foodkitdb;
-psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE foodkitdb TO foodkit;";
-psql -U postgres -c "CREATE EXTENSION postgis;" foodkitdb;
-psql -U postgres -c "CREATE EXTENSION postgis_topology;" foodkitdb;
